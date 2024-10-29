bf77d0a2bb49b356a6b49392f3b136a765e7de80
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-10-29 11:23:38 +0600

    first commit

---
+++ authorization.js
@@ -0,0 +1,1 @@
+console.log("How old are you?");
---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello!");
+
+function getDate() {
+  console.log(Date());
+}
+function showMessage() {
+  console.log("Hello world");
+}
+function changeColor() {
+  document.getElementById("logo").style.color = "blue";
+  console.log("Nazar!");
+}
+function reset() {
+  document.getElementById("logo").style.color = "";
+}
