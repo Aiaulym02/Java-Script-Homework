f0e3d0eab5075a78334c6c64073977c139d2613a
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-10-30 11:18:51 +0600

    var name(қолданылуы)

---
+++ examples/js.js
@@ -0,0 +1,15 @@
+var name = "AIAULYM"; // Ауыспалы айнымалы
+
+name = "Gulaida";
+
+name = "Eldos";
+
+name = "Mukhanmmedali";
+
+const phone = "192.168.123.10"; // Тұрақты
+
+phone = "192.172.35.10";
+
+console.log(phone); // Қате қайтарады
+
+document.getElementById("username").innerHTML = name;
