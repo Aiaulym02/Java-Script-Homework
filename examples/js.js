4cd9564fa2aac02b5fd05cb29a186a06fbd12df8
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-10-30 11:45:39 +0600

    var name,console.loge

--- examples/js.js
+++ examples/js.js
@@ -13,3 +13,15 @@ phone = "192.172.35.10";
 console.log(phone); // Қате қайтарады
 
 document.getElementById("username").innerHTML = name;
+
+var streeetName = "Maulenov,92"; // String
+var currentDay = 30; // Number
+var AmirKeldi = false; // Boolean true=шындық,false=өтірік
+var JanaSumka = null; // Бос мән
+var porsche = undefined; //Жоқ айнымалы
+
+console.log("Көше", streeetName);
+console.log("Қазіргі күн", currentDay);
+console.log("Әмір келді!", AmirKeldi);
+console.log("Жаңа сатып алған сумканың ішіндегі заттар", JanaSumka);
+console.log("Порше бар", porsche);
