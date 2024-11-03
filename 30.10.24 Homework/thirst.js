348dd01d1b1e0750e18e8a4027d92554465b66ef
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-11-03 22:32:42 +0600

    Homework

---
+++ 30.10.24 Homework/thirst.js
@@ -0,0 +1,11 @@
+var modelName = "Daewoo Matiz";
+var color = "green";
+var wheelsCount = 4;
+var is0ffRoad = false;
+var rating = 7;
+
+console.log("Модель атауы", modelName);
+console.log("Түсі", color);
+console.log("Дөңгелек саны", wheelsCount);
+console.log("Жолсыз аймақпен (бездорожье) жүруге арналған ба ", is0ffRoad);
+console.log("Рейтингі", rating);
