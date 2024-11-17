79397455143a241b7aca54dec0909bfcafc24094
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-11-17 19:35:54 +0600

    Homework

---
+++ 11.11.24 Homework/java11.js
@@ -0,0 +1,7 @@
+var Number = prompt("Үш (a,b,c) сан енгізіңіз");
+
+if (Number == a + b > c || a + c > b || b + c > a) {
+  alert("Бұл үшбұрыш жасай алады");
+} else if (Number != a + b > c || a + c > b || b + c > a) {
+  alert("Бұл үшбұрыш жасай алмайды");
+}
