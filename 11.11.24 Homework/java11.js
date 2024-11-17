528bfe92f5f9c9822de628770c4d6f5690317e89
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-11-17 20:11:22 +0600

    Homework

--- 11.11.24 Homework/java11.js
+++ 11.11.24 Homework/java11.js
@@ -1,7 +1,32 @@
+//1
 var Number = prompt("Үш (a,b,c) сан енгізіңіз");
 
-if (Number == a + b > c || a + c > b || b + c > a) {
+if (Number == a + b > c  b + c > a) {
   alert("Бұл үшбұрыш жасай алады");
-} else if (Number != a + b > c || a + c > b || b + c > a) {
+} else if (Number != a + b > c  b + c > a) {
   alert("Бұл үшбұрыш жасай алмайды");
+
+//3
+let upai = prompt("0-ден 100-ге дейін ұпай енгізіңіз");
+switch (upai) {
+  case "90-100":
+    alert("Баға:A");
+    break;
+  case "80-89":
+    alert("Баға: B");
+    break;
+  case "70-79":
+    alert("Баға: C");
+    break;
+  case "60 - 69":
+    alert("Баға: D");
+    break;
+  case "0-59":
+    alert("Баға: F");
+    break;
 }
+//90-100: "Баға: А"
+//80-89: "Баға: B"
+//70-79: "Баға: C"
+//60-69: "Баға: D"
+//0-59: "Баға: F"
