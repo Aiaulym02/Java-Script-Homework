4f05a88b88d0947fb2aba18404f1f03b532de097
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-11-24 19:25:41 +0600

    Homework

--- 11.11.24 Homework/java11.js
+++ 11.11.24 Homework/java11.js
@@ -1,32 +1,30 @@
-//1
-var Number = prompt("Үш (a,b,c) сан енгізіңіз");
-
-if (Number == a + b > c  b + c > a) {
+// Есеп 1
+let a = prompt("Birinshi san");
+let b = prompt("Ekinshi san");
+let c = prompt("Ushinshi san");
+if (a + b > c || a + c > b || b + c > a) {
   alert("Бұл үшбұрыш жасай алады");
-} else if (Number != a + b > c  b + c > a) {
-  alert("Бұл үшбұрыш жасай алмайды");
-
-//3
+} else {
+  ("Бұл үшбұрыш жасай алмайды");
+}
+//Есеп 3
 let upai = prompt("0-ден 100-ге дейін ұпай енгізіңіз");
 switch (upai) {
-  case "90-100":
-    alert("Баға:A");
-    break;
-  case "80-89":
-    alert("Баға: B");
-    break;
-  case "70-79":
-    alert("Баға: C");
+  case "0-59":
+    alert("Баға: F");
     break;
   case "60 - 69":
     alert("Баға: D");
     break;
-  case "0-59":
-    alert("Баға: F");
+  case "70-79":
+    alert("Баға: C");
     break;
+  case "80-89":
+    alert("Баға: B");
+    break;
+  case "90-100":
+    alert("Баға:A");
+    break;
+  default:
+    alert("0-ден 100-ге дейін ұпай енгізіңіз");
 }
-//90-100: "Баға: А"
-//80-89: "Баға: B"
-//70-79: "Баға: C"
-//60-69: "Баға: D"
-//0-59: "Баға: F"
