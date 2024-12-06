9e32707d662323f8be9386971d7679560835bc90
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-12-06 19:31:50 +0600

    Homework

---
+++ 20.11.24 Homework/js20.js
@@ -0,0 +1,21 @@
+//1esep
+// function ggg() {
+//   let num = +prompt("Bir san engiz");
+//   if (num % 2 == 0) {
+//     alert("bul san zhup");
+//   } else {
+//     alert("bul san taq");
+//   }
+// }
+// alert(ggg());
+
+//2
+function kosu() {
+  let num = 1;
+  for (let i = 1; 1 < 100; i++) {
+    num = num + i;
+  }
+  console.log(i);
+}
+
+kosu();
