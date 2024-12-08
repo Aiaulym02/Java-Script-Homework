2cf98ac8f087a2b7c4868fce09ad6ddf467a7762
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-12-08 19:31:49 +0600

    Homework

--- 03.12 Homework/java3.js
+++ 03.12 Homework/java3.js
@@ -1,20 +1,31 @@
 //  №1
 const city = {
   name: "Almaty",
-  population: "2 млн астам",
+  population: "2 млн +",
   districts: [
-    { name: "Еңбекшіқазақ ауданы", population: "300427 адам (2019)" },
+    { name: "Алмалинский район", population: "262561" },
+    { name: "Алатауский район", population: "289150" },
+    { name: "Медеуский район", population: "241263" },
   ],
 };
+console.log(city.districts);
 
-console.log(city);
-
-//  №2
+//№2
 const schools = [
   {
-    name: "Ж.Қайыпов атындағы орта мектеп",
-    district: "Еңбекшіқазақ ауданы,Қазақстан ауылы",
-    students: 900,
+    name: "Школа-гимназия №136",
+    district: "Алмалинский район",
+    students: 1500,
+  },
+  {
+    name: "Общеобразовательная №205",
+    district: "Алатауский район",
+    students: 2000,
+  },
+  {
+    name: "Сенім частная школа",
+    district: "Медеуский район",
+    students: 1278,
   },
 ];
 console.log(schools);
