442ae4f2aa45460c669eff0db389daba7f027832
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-12-08 18:22:29 +0600

    Homework

---
+++ 02.12 Homework/java2.js
@@ -0,0 +1,24 @@
+// title (мәтін) – кітаптың атауы
+// author (мәтін) – авторы
+// pages (сан) – беттер саны
+// isRead (логикалық(true/false)) – кітап оқылғанын білдіретін белгі
+// readBook(): Консольге хабарлама шығарады:
+// "Сіз [title] кітабын [author] жазған оқып жатырсыз."
+// toggleReadStatus(): isRead қасиетінің мәнін керісінше өзгертеді және мынадай хабарлама шығарады:
+// "Оқу күйі жаңартылды: [true/false]."
+
+const librarry = {
+  title: "Менің атым Қожа",
+  author: "Бердібек Соқпақбаев",
+  pages: 150,
+  isRead: false,
+  addBooks: function () {
+    this.isRead = !this.isRead;
+    return this.isRead;
+  },
+  addLibrarry: function () {
+    return `Сіз ${this.author} авторының ${this.title} кітабын оқып жатырсыз!`;
+  },
+};
+
+console.log(librarry.addBooks());
