6cee1610a342a3fb2548b52bf70a790005717c5b
aiaulym <aiaulymrakhimberdi02@gmail.com>
2024-12-15 18:00:33 +0600

    homework

--- 06.12 Homework/java6.js
+++ 06.12 Homework/java6.js
@@ -1,15 +1,39 @@
 //3
-const students = {
-  student: [
-    { name: "Aruzhan", age: 21 },
-    { name: "Enlik", age: 14 },
-    { name: "Zhanar", age: 26 },
-    { name: "Altynai", age: 19 },
-    { name: "Aiaulym", age: 32 },
-  ],
-};
-function enUlkenstudent() {
-  return students.sort(students.age > 30);
-  alert(enUlkenstudent);
+const students = [
+  {
+    name: "Aigul",
+    age: 17,
+    grade: "10 B",
+  },
+  {
+    name: "Ruslan",
+    age: 9,
+    grade: "4C",
+  },
+  {
+    name: "Zhanar",
+    age: 18,
+    grade: "11",
+  },
+  {
+    name: "Marzhan",
+    age: 11,
+    grade: "5A",
+  },
+  {
+    name: "Samat",
+    age: 12,
+    grade: "7Б",
+  },
+];
+console.log(students);
+
+function addStudents() {
+  return students.sort((a, b) => a.age - b.age)[students.length - 1];
 }
-enUlkenstudent();
+console.log(addStudents());
+
+function maxStudent() {
+  return students.filter((num) => num.age <= 14);
+}
+console.log(maxStudent());
