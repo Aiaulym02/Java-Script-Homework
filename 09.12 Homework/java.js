ac1c8f0e43880aff74cb63b8b065a54b3adb867f
aiaulym <aiaulymrakhimberdi02@gmail.com>
2025-01-19 23:07:33 +0600

    Homework

---
+++ 09.12 Homework/java.js
@@ -0,0 +1,29 @@
+const library = {
+  name: "Орталық кітапхана",
+  address: "Алматы қаласы, Абай көшесі, 45",
+  books: [
+    {
+      title: "Жүз жылдық жалғыздық",
+      author: "Габриэль Гарсиа Маркес",
+      year: 1967,
+      isAvailable: true,
+    },
+    {
+      title: "Сиқырлы тау",
+      author: "Томас Манн",
+      year: 1924,
+      isAvailable: false,
+    },
+    {
+      title: "Соғыс және бейбітшілік",
+      author: "Лев Толстой",
+      year: 1869,
+      isAvailable: true,
+    },
+  ],
+};
+
+function changeLibraryName() {
+  let newName = prompt("Zhana kitaphana atauyn engiziniz");
+  
+}
